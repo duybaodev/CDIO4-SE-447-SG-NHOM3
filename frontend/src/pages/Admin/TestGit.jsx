e744ebd42d726dad21c5f9989645df22f6215de0
haitchdo287_aitchdo287ops <haitchdo287@gmail.com>
2026-07-23 21:03:19 +0700

    TestGit.TestGit.jsx

---
+++ frontend/src/pages/Admin/TestGit.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestGit = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Trang thử nghiệm đẩy code Git mới</h2>
+      <p>File này được tạo riêng để test push/pull code.</p>
+    </div>
+  );
+};
+
+export default TestGit;
